7c131e492cf66f7b283fbce0687bdc9c81e9c5b5
OhMinSeop <mins5190@gmail.com>
2022-02-19 16:39:01 +0900

    mod: template scripts에서 환경 및 빌드 방법 변경

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,4 @@
-import multiavatar from '@multiavatar/multiavatar/esm';
 import qs from 'qs';
-import { STORAGE_KEY } from '@contants/constant';
 import axios, { AxiosError } from 'axios';
 
 export const makeQueryString = (params: any) =>
@@ -15,9 +13,9 @@ export const getUniqueFilter = (iters: { [key: string]: any }[], key: string) =>
     iters.reduce((map, obj) => map.set(obj[key], obj), new Map()).values(),
   );
 
-export function isAxiosError<R = any>(
+export function isAxiosError<T = any, D = any>(
   error: AxiosError | any,
-): error is AxiosError<R> {
+): error is AxiosError<T, D> {
   return error && axios.isAxiosError(error);
 }
 
@@ -26,52 +24,6 @@ export const delay = (ms: number) =>
 
 export const generateKey = () => {
   return Math.random().toString(36).substr(2, 11);
-};
-
-export const generateAvatar = (avatarKey: string) => {
-  return multiavatar(avatarKey);
-};
-
-export const getToken = () => {
-  if (!isBrowser) return '';
-  const token = localStorage.getItem(STORAGE_KEY.TOKEN_KEY);
-  if (!token) return '';
-  return token;
-};
-
-export const getUserInfo = () => {
-  if (!isBrowser) return null;
-  const token = getToken();
-  if (!token) return null;
-
-  try {
-    const stringify = localStorage.getItem(STORAGE_KEY.USER_KEY);
-    if (!stringify) return null;
-
-    const userInfo = JSON.parse(stringify);
-    if (!userInfo) return null;
-
-    return userInfo;
-  } catch (error) {
-    return null;
-  }
-};
-
-export const getUserThumbnail = ({
-  defaultProfile,
-  avatarSvg,
-  profileUrl,
-  name = 'null',
-}: {
-  defaultProfile: boolean;
-  avatarSvg?: string | null;
-  profileUrl?: string | null;
-  name?: string;
-}) => {
-  const svgCode = `data:image/svg+xml;utf8,${encodeURIComponent(
-    generateAvatar(avatarSvg ?? name),
-  )}`;
-  return defaultProfile ? svgCode : profileUrl ?? svgCode;
 };
 
 export function canUseDOM(): boolean {
